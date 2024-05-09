2a33f81fe25dbfabeefc93a793783f5942c207ee
ShresthaPrajwal <pjkoju@gmail.com>
2024-05-09 16:32:37 +0545

    updated mediaController getAllMedia function

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser')
 
-const mongoose = require('mongoose');
-const config = require('./config/config');
-const logger = require('./utils/logger');
+const mongoose = require('./services/db');
 const middleware = require('./middlewares/middleware');
 const errorHandler = require('./middlewares/errorHandler')
 const swaggerSetup = require('./config/swagger')
@@ -13,19 +11,7 @@ const router = require('./routers/index')
 
 const app = express();
 
-mongoose.set('strictQuery', false);
-
-mongoose
-  .connect(config.MONGO_URI)
-  .then(() => {
-    logger.info('Connected to MongoDB');
-  })
-  .catch((error) => {
-    logger.error('Error Connecting to MongoDB', error.message);
-  });
-
-
-app.use(express.static('/upload'));
+app.use(express.static('uploads'));
 app.use(cors());
 app.use(express.json());
 app.use(middleware.requestLogger);
