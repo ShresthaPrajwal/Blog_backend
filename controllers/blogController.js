ca0a3272b94d4128093428aed4e6c015579b8f88
ShresthaPrajwal <pjkoju@gmail.com>
2024-05-10 15:32:45 +0545

    Added getBlogs with url util

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,46 +1,37 @@
 const Blog = require('../models/blogModel');
-const getMediaWithUrls = require('../utils/getMedia');
-
+const getBlogWithUrl = require('../utils/getBlogs');
 
 async function uploadBlog(req, res, next) {
   try {
     const newBlog = new Blog();
-    console.log(req.body)
+    console.log(req.body);
     newBlog.title = req.body.title || 'Title';
     newBlog.content = req.body.content || 'Content';
     newBlog.featuredImage = req.body.featuredImage;
     newBlog.media = req.body.media;
 
     let savedBlog = await newBlog.save();
-    savedBlog = await Blog.findById(savedBlog._id).populate('media').populate('featuredImage');
-    if(savedBlog instanceof Blog) savedBlog=savedBlog.toObject()
-    const savedBlogWithUpdatedUrl = {...savedBlog,media:getMediaWithUrls(req,savedBlog.media,next)}
+    savedBlog = await Blog.findById(savedBlog._id)
+      .populate('media')
+      .populate('featuredImage');
+
+    const savedBlogWithUpdatedUrl = getBlogWithUrl(req,savedBlog,next)
 
     res.status(201).json({
       message: 'Blog uploaded successfully!',
-      success:true,
-      data: savedBlogWithUpdatedUrl
+      success: true,
+      data: savedBlogWithUpdatedUrl,
     });
   } catch (err) {
     next(error);
   }
 }
 
-async function getAllBlogs(req,res,next){
+async function getAllBlogs(req, res, next) {
   try {
     const blogs = await Blog.find().populate('media').populate('featuredImage');
+    const blogsWithUpdatedUrl = getBlogWithUrl(req, blogs, next);
 
-    const blogsWithUpdatedUrl = blogs.map((blog)=>{
-      if (blog instanceof Blog) blog = blog.toObject();
-      const blogWithUpdatedUrl = {
-        ...blog,
-        media: getMediaWithUrls(req, blog.media, next),
-      };
-      return blogWithUpdatedUrl
-    })
-
-    
-    
     res.status(200).json({
       success: true,
       data: blogsWithUpdatedUrl,
@@ -50,4 +41,26 @@ async function getAllBlogs(req,res,next){
   }
 }
 
-module.exports = { uploadBlog, getAllBlogs };
+async function getBlogById(req, res, next) {
+  try {
+    const blogId = req.params.id;
+    const blog = await Blog.findById(blogId)
+      .populate('media')
+      .populate('featuredImage');
+    if (!blog) {
+      return res.status(404).json({
+        success: false,
+        message: 'Blog not found',
+      });
+    }
+    res.status(200).json({
+      success: true,
+      data: blog,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+
+module.exports = { uploadBlog, getAllBlogs, getBlogById };
