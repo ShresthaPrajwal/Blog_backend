0fe65d4f569138094e1b047313b58cfa2366bb54
ShresthaPrajwal <pjkoju@gmail.com>
2024-05-12 23:34:15 +0545

    Multer file count error handler added

--- utils/multerError.js
+++ utils/multerError.js
@@ -3,14 +3,22 @@ const multer = require('multer');
 const multerErrorHandler = (err, req, res, next) => {
   console.log('From multerErrorHandler', req.files);
   if (err instanceof multer.MulterError) {
-    if (err.code === 'LIMIT_FILE_SIZE') {
-      return res
-        .status(400)
-        .json({ message: 'File size limit exceeded', error: err.message });
+    switch (err.code) {
+      case 'LIMIT_FILE_SIZE':
+        return res
+          .status(400)
+          .json({ message: 'File size limit exceeded', error: err.message });
+
+      case 'LIMIT_FILE_COUNT':
+        return res
+          .status(400)
+          .json({ message: 'Too many Files', error: err.message });
+
+      default:
+        return res
+          .status(500)
+          .json({ message: 'Multer error', error: err.message });
     }
-    return res
-      .status(500)
-      .json({ message: 'Multer error', error: err.message });
   }
   if (err) {
     return res
