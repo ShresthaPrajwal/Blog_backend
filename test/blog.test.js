f59274c9a40e42717e973c51a49b693025dc5182
ShresthaPrajwal <pjkoju@gmail.com>
2024-05-17 13:05:38 +0545

    Separated test files

---
+++ test/blog.test.js
@@ -0,0 +1,94 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const { expect } = chai;
+const app = require('../app');
+const Media = require('../models/mediaModel');
+const User = require('../models/usersModel');
+const Blog = require('../models/blogModel');
+chai.use(chaiHttp);
+const bcrypt = require('bcrypt');
+const path = require('path');
+
+describe('Blog API', () => {
+  let token;
+  let uploadedMediaId;
+  before(async function () {
+    await User.deleteMany({});
+    await User.create({
+      username: 'testuser',
+      passwordHash: await bcrypt.hash('password123', 10),
+      name: 'Test User',
+    });
+
+    const res = await chai.request(app).post('/api/login').send({
+      username: 'testuser',
+      password: 'password123',
+    });
+    const filePath = path.join('./public', 'sign.jpg');
+    token = res.body.token;
+
+    const res2 = await chai
+      .request(app)
+      .post('/api/media')
+      .set('Authorization', `Bearer ${token}`)
+      .attach('image', filePath);
+    console.log(res2.body)
+    uploadedMediaId = res2.body.results[0]._id;
+  });
+  after(async function () {
+    User.deleteMany({});
+    Media.deleteMany({});
+    Blog.deleteMany({});
+  });
+
+  it('should upload a blog successfully', async () => {
+    const blogData = {
+      title: 'Test Blog Title',
+      content: 'This is some blog content',
+      featuredImage: uploadedMediaId,
+      media: [uploadedMediaId],
+    };
+    console.log('from blg test', uploadedMediaId, blogData);
+    const res = await chai
+      .request(app)
+      .post('/api/blogs')
+      .set('Authorization', `Bearer ${token}`)
+      .send(blogData);
+
+    expect(res).to.have.status(201);
+    expect(res.body).to.have.property('message', 'Blog uploaded successfully!');
+    expect(res.body.data).to.have.property('title', blogData.title);
+    expect(res.body.data).to.have.property('featuredImage');
+    console.log('from blog test post', res.body);
+  });
+
+  it('get all blogs succesfully', async () => {
+    const res = await chai.request(app).get('/api/blogs');
+
+    expect(res).to.have.status(200);
+    expect(res.body).to.have.property('pagination');
+    expect(res.body.data).to.be.an('array');
+  });
+
+  it('should get a blog by slug successfully', async () => {
+    const blogData = {
+      title: 'Test Blog Title',
+      content: 'This is some blog content',
+      featuredImage: uploadedMediaId,
+      media: [uploadedMediaId],
+    };
+
+    const blogCreationRes = await chai
+      .request(app)
+      .post('/api/blogs')
+      .set('Authorization', `Bearer ${token}`)
+      .send(blogData);
+    const createdBlog = blogCreationRes.body.data;
+    const res = await chai.request(app).get(`/api/blogs/${createdBlog.slug}`);
+
+    expect(res).to.have.status(200);
+    expect(res.body.data).to.have.property('title', blogData.title);
+    expect(res.body.data).to.have.property('featuredImage');
+    expect(res.body.data.media).to.be.an('array');
+  });
+});
---
+++ test/media.test.js
@@ -0,0 +1,88 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const { expect } = chai;
+const app = require('../app');
+const Media = require('../models/mediaModel');
+const User = require('../models/usersModel');
+chai.use(chaiHttp);
+const bcrypt = require('bcrypt');
+const path = require('path')
+
+describe('Media API', () => {
+  let token;
+  let uploadedMediaId;
+  before(async function () {
+    await User.deleteMany({});
+    await User.create({
+      username: 'testuser',
+      passwordHash: await bcrypt.hash('password123', 10),
+      name: 'Test User',
+    });
+
+    const res = await chai.request(app).post('/api/login').send({
+      username: 'testuser',
+      password: 'password123',
+    });
+
+    token = res.body.token;
+  });
+
+  after(async function () {
+    User.deleteMany({});
+    Media.deleteMany({});
+  });
+
+  it('should upload media successfully', async () => {
+    const filePath = path.join('./public', 'sign.jpg');
+
+    const res = await chai
+      .request(app)
+      .post('/api/media')
+      .set('Authorization', `Bearer ${token}`)
+      .attach('image', filePath);
+
+    expect(res).to.have.status(201);
+    expect(res.body).to.have.property('message', 'Media Uploaded Successfully');
+    uploadedMediaId = res.body.results[0]._id;
+  });
+
+  it('should get media by ID successfully', async () => {
+    const res = await chai.request(app).get(`/api/media/${uploadedMediaId}`);
+    expect(res).to.have.status(200);
+    expect(res.body).to.be.a('object');
+  });
+
+  it('should get all media successfully', async () => {
+    const res = await chai.request(app).get('/api/media');
+
+    expect(res).to.have.status(200);
+    expect(res.body).to.have.property('message', 'All Media Found');
+    expect(res.body.results).to.be.an('array');
+  });
+
+  it('should edit media successfully', async () => {
+    const newCaption = 'This is an edited caption';
+    const newAlternateText = 'Edited alternate text';
+
+    const res = await chai
+      .request(app)
+      .put(`/api/media/${uploadedMediaId}`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({ caption: newCaption, alternateText: newAlternateText });
+
+    expect(res).to.have.status(200);
+    expect(res.body).to.have.property('message', 'Media Updated Successfully');
+    expect(res.body.results.caption).to.equal(newCaption);
+    expect(res.body.results.alternateText).to.equal(newAlternateText);
+  });
+
+  it('should delete media successfully', async () => {
+    const res = await chai
+      .request(app)
+      .delete(`/api/media/${uploadedMediaId}`)
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(res).to.have.status(200);
+    expect(res.body).to.have.property('message', 'Media Deleted Succesfully');
+  });
+});
